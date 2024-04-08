da18bd5e264cb70e63dbd164090a7f1786703334
ck-ngetich <kiprongetich3@gmail.com>
2024-04-08 16:38:10 -0700

    New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function that create a binary tree code
+ * @parent: pointer to the parent of the node to create.
+ * @value: value to put in the new node.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_val;
+
+	new_val = malloc(sizeof(binary_tree_t));
+	if (new_val == NULL)
+		return (NULL);
+
+	new_val->n = value;
+	new_val->parent = parent;
+	new_val->left = NULL;
+	new_val->right = NULL;
+
+	return (new_val);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is 0x1D. C - Binary trees project under ALX Program
